58bdf70c716109733626d6c9ec1dd22a967230b0
Gurveer Singh <145554336+Gurveer1510@users.noreply.github.com>
2024-11-07 23:39:22 +0530

    Update Profile.tsx

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -23,7 +23,7 @@ const Profile = () => {
                     </div>
                 <div className='w-screen p-12 pt-2 md:pt-4'>
                     <div>
-                        <div className="self-start text-xl font-semibold border-b">
+                        <div className="pb-2 md:pb-4 self-start text-xl font-semibold border-b-2">
                             Your stories: {blogs && blogs?.length > 0 ? blogs?.length : 0}
                         </div>
                         <BlogsOnProfile
